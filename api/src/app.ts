dd003a430cca8c5381a3f0db000efd4990562a68
rayhankinan <rayhankinan@gmail.com>
2023-01-11 00:29:44 +0700

    Add path to typescript

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,11 @@
 import express, { Express } from "express";
 import { DataSource } from "typeorm";
+import cors from "cors";
+import mongoose from "mongoose";
+import serverConfig from "@config/server";
+import corsConfig from "@config/cors";
+import postgresConfig from "@config/postgres";
+import mongoConfig from "@config/mongo";
 import "reflect-metadata";
 
 class App {
@@ -8,10 +14,26 @@ class App {
 
     constructor() {
         this.server = express();
+        this.dataSource = new DataSource(postgresConfig);
+
+        this.server.use(
+            express.json(),
+            express.urlencoded({ extended: true }),
+            cors(corsConfig)
+        );
     }
 
     async run() {
-        this.server.listen(3000);
+        await Promise.all([
+            this.dataSource.initialize(),
+            mongoose.connect(mongoConfig.URI),
+        ]);
+
+        this.server.listen(serverConfig.port, () => {
+            console.log(
+                `Server is running on http://${serverConfig.host}:${serverConfig.port}`
+            );
+        });
     }
 }
 

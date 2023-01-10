381a1ba53a4cd9838ce2d54d9e9679ab54a8e56a
rayhankinan <rayhankinan@gmail.com>
2023-01-10 19:00:08 +0700

    Initialize file

---
+++ client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import 'vite/client'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: parseInt(import.meta.env.VITE_PORT) || 3000
+  }
+})

07c101f7f8fa964507032b23eae1aeee33bbdd67
rayhankinan <rayhankinan@gmail.com>
2023-01-11 22:09:45 +0700

    Add some entities

--- api/src/entities/postgresql/major.ts
+++ api/src/entities/postgresql/major.ts
@@ -1,17 +1,24 @@
-import { Column, Entity, ManyToOne } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
 import BasePostgreSQL from "@entities/postgresql/base";
 import Admin from "@entities/postgresql/admin";
 import Faculty from "@enum/faculty";
 
 @Entity()
 class Major extends BasePostgreSQL {
-    @Column()
+    @Column({ unique: true })
+    public prefix: number;
+
+    @Column({ unique: true })
     public name: string;
 
     @Column({ type: "enum", enum: Faculty })
     public faculty: Faculty;
 
+    @Column()
+    public adminId: number;
+
     @ManyToOne(() => Admin, (admin) => admin.id, { cascade: true })
+    @JoinColumn({ name: "adminId" })
     public admin: Promise<Admin>;
 }
 
--- api/src/enum/faculty.ts
+++ api/src/enum/faculty.ts
@@ -1,3 +1,16 @@
-enum Faculty {}
+enum Faculty {
+    FMIPA = "FMIPA",
+    SITH = "SITH",
+    SF = "SF",
+    FTTM = "FTTM",
+    FITB = "FITB",
+    FTI = "FTI",
+    STEI = "STEI",
+    FTMD = "FTMD",
+    FTSL = "FTSL",
+    SAPPK = "SAPPK",
+    FSRD = "FSRD",
+    SBM = "SBM",
+}
 
 export default Faculty;
